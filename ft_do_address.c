96616b0c3d2875beb26c3bf198d7e867db0d4eec
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-18 22:11:26 +0100

    fix null at %p or just make it nil

--- ft_do_address.c
+++ ft_do_address.c
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 21:23:09 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/18 21:43:52 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/18 22:10:52 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	ft_do_address(va_list *list)
 {
-	if (*list == NULL)
-	{
-		ft_putstr("(null)");
-		return (6);
-	}
 	unsigned long	addr;
 
 	addr = (unsigned long)va_arg(*list,void*);
 
+	if (addr == 0)
+	{
+		ft_putstr("(nil)");
+		return 4;
+	}
 	ft_putstr("0x");
 	ft_print_hex(addr,0);
 	return (2 + ft_count_unsigned_base(addr,16));	
