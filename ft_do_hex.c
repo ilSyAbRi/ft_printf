dbe06a3b6a18f10e3d4b9d3adb053afd5a7f633e
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-18 19:28:24 +0100

    remove 0X and 0x from hex

--- ft_do_hex.c
+++ ft_do_hex.c
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 19:04:00 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/18 19:23:13 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/18 19:27:32 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,11 @@ int	ft_do_hex(char c, va_list *list)
 	keep = va_arg(*list,unsigned long);
 	if(c == 'x')
 	{
-		ft_putstr("0x");
 		ft_print_hex(keep,0);
 		return (2 + ft_count_unsigned_base(keep,16));
 	}
 	else if (c == 'X')
 	{
-		ft_putstr("0x");
 		ft_print_hex(keep,1);
 		return (2 + ft_count_unsigned_base(keep,16));
 	}
