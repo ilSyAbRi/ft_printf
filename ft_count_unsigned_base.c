aecf6cc08a268afc28e141a799e3afb49abd54a5
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-20 15:30:26 +0100

    small improvement

--- ft_count_unsigned_base.c
+++ ft_count_unsigned_base.c
@@ -6,11 +6,9 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 14:40:01 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/18 22:39:15 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/20 15:26:35 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "ft_printf.h"
 
 int	ft_count_unsigned_base(unsigned long nb, int base)
 {
